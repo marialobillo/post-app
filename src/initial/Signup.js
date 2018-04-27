e99d99bdfff10bb2a6510e582309a608912e9a78
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-27 16:27:07 +0200

    using the form

--- src/initial/Signup.js
+++ src/initial/Signup.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ContactForm from './Form';
 
 const Signup = () => {
+	const functionForma = (datos) => {
+		console.log(datos);
+	}
 	return (
 		<div>
 			<h2>Signup</h2>
+			<ContactForm onSubmit={functionForma} />
 		</div>
 	);
 }
